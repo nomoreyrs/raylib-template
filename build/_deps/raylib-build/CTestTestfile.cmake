40f4fa599a95f10d84b7bbafbb2fd49958c13f70
no more yrs <176704234+nomoreyrs@users.noreply.github.com>
2025-06-09 03:08:57 -0400

    init

--- build/_deps/raylib-build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/robert/.cache/cpm/raylib/cebddbc4c62ca18a6ec46d70429891bdbc16af23
-# Build directory: C:/Code/C-CPP/Raylib/my-raylib-template/build/_deps/raylib-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("raylib")
